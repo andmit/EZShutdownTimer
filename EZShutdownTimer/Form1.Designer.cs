0f1887550bb6cd13211545e77ec61aa869d176c9
Andrew Mitchell <32050288+andmit@users.noreply.github.com>
2019-01-08 09:11:23 -0800

    EZShutdownTimer
    This program shuts down the computer on a timer :pig:

--- EZShutdownTimer/Form1.Designer.cs
+++ EZShutdownTimer/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.secs = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 54);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // secs
+            // 
+            this.secs.AutoSize = true;
+            this.secs.Location = new System.Drawing.Point(150, 60);
+            this.secs.Name = "secs";
+            this.secs.Size = new System.Drawing.Size(13, 13);
+            this.secs.TabIndex = 1;
+            this.secs.Text = "0";
+            this.secs.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(581, 222);
+            this.Controls.Add(this.secs);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label secs;
     }
 }
 
