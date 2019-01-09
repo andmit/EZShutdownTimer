d144653e2bf58191aef67b0c2abab0e11bc03b87
Andrew Mitchell <32050288+andmit@users.noreply.github.com>
2019-01-09 09:14:03 -0800

    Implemented Correctly
    Was working in inital tests now it's dead

--- EZShutdownTimer/Form1.Designer.cs
+++ EZShutdownTimer/Form1.Designer.cs
@@ -31,21 +31,28 @@
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.secs = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(12, 54);
+            this.numericUpDown1.DecimalPlaces = 3;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 12);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(173, 20);
             this.numericUpDown1.TabIndex = 0;
             this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // secs
             // 
             this.secs.AutoSize = true;
-            this.secs.Location = new System.Drawing.Point(150, 60);
+            this.secs.Location = new System.Drawing.Point(191, 14);
             this.secs.Name = "secs";
             this.secs.Size = new System.Drawing.Size(13, 13);
             this.secs.TabIndex = 1;
@@ -55,7 +62,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 81);
+            this.button1.Location = new System.Drawing.Point(17, 38);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -63,11 +70,23 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(110, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Abort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(252, 156);
+            this.ClientSize = new System.Drawing.Size(268, 69);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.secs);
             this.Controls.Add(this.numericUpDown1);
@@ -84,6 +103,7 @@
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Label secs;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- EZShutdownTimer/Form1.cs
+++ EZShutdownTimer/Form1.cs
@@ -25,31 +25,49 @@ namespace EZShutdownTimer
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
-            int seconds = (int)(numericUpDown1.Value * 60) * 60;
+            decimal seconds = (numericUpDown1.Value * 60) * 60;
             secs.Text = seconds + "Secs";
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Process cmd = new Process();
-            cmd.StartInfo.FileName = "cmd.exe";
-            cmd.StartInfo.RedirectStandardInput = true;
-            cmd.StartInfo.RedirectStandardOutput = true;
-            cmd.StartInfo.CreateNoWindow = true;
-            cmd.StartInfo.UseShellExecute = false;
-            cmd.Start();
 
-            cmd.StandardInput.WriteLine("shutdown -s -t" + secsGet());
-            cmd.StandardInput.Flush();
-            cmd.StandardInput.Close();
-            cmd.WaitForExit();
-            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+            shutdown(true);
         }
 
-        private int secsGet()
+        private void shutdown(bool state)
         {
-            return 1 + (int)numericUpDown1.Value * 60 * 60;
+            if (state)
+            {
+                var pwr = new ProcessStartInfo("shutdown", "/s /t " + secsGet());
+
+                pwr.CreateNoWindow = true;
+                pwr.UseShellExecute = false;
+                Process.Start(pwr);
+                button1.Visible = false;
+                button2.Visible = true;
+            }
+            else {
+                var pwr = new ProcessStartInfo("shutdown", "/a");
+
+                pwr.CreateNoWindow = true;
+                pwr.UseShellExecute = false;
+                Process.Start(pwr);
+                button1.Visible = true;
+                button2.Visible = false;
+                
+            }
+        }
+
+        private decimal secsGet()
+        {
+            return 1 + numericUpDown1.Value * 60 * 60;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            shutdown(false);
         }
     }
 }
